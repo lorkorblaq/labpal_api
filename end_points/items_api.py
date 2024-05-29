d54c19b1432bef816b3842e0504710028492d2f2
lorkorblaq <f3miolok@gmail.com>
2024-05-29 14:34:35 +0100

    update for requistion

--- end_points/items_api.py
+++ end_points/items_api.py
@@ -25,6 +25,7 @@ class ItemsResource(Resource):
         # Create a Flask response with JSON data
         # print((response))
         return response_data, 200
+
 class ItemsBulkPush(Resource):
     def post(self):
         try:
@@ -47,7 +48,6 @@ class ItemsBulkPush(Resource):
         except Exception as e:
             abort(500, message=str(e))
 
-
 class ItemsPut(Resource):
     def put(self):
         # utc_now = datetime.utcnow()
@@ -64,8 +64,7 @@ class ItemsPut(Resource):
             ITEMS_COLLECTION.update_one(filter, new_value)
         response = {"message": "Your data has been updated successfully",}
         return response, 200
-        
-
+ 
 def requiste(bench, days, categories):
     query = {}
     if bench:
@@ -73,50 +72,44 @@ def requiste(bench, days, categories):
     if categories:
         query["category"] = {"$in": categories}
     print(query)
-    ITEMS_COLLECTION.find(query, { "bench":1, "item":1, "in stock":1, "tests/day":1, "tests/vial":1 })
+    
     pipeline = [
-            # Match documents based on the bench field
-            {"$match": query},
-            # Project the fields needed for calculations
-            {"$project": {
-                "bench":1,
-                "item":1,
-                "in stock":1,
-                "total_tests_in_stock": {"$multiply": ["$in stock", "$tests/vial"]},
-                "tests_per_day": {"$ceil":"$tests/day"},
-                "tests/vial":1
-            }},
-            {"$addFields": {
-                # "quantity_needed": {"$ceil": {"$divide": [days, "$tests_per_day"]}},
-                "total_tests_in_stock": {"$multiply": ["$in stock", "$tests/vial"]},
-                "total_days_to_last": {"$ceil": {"$divide": ["$total_tests_in_stock", "$tests_per_day"]}},
-            
-                "quantity_test_requested": {"$multiply": ["$tests_per_day", days]},
-                # "total_days_to_last": "$total_days_to_last",  # Include total_days_last in the final result
-                "total_tests_in_stock": "$total_tests_in_stock"
-            }}
-        ]
-        # Execute the aggregation pipeline
+        # Match documents based on the bench field
+        {"$match": query},
+        # Project the fields needed for calculations and convert to numeric types
+        {"$project": {
+            "bench": 1,
+            "item": 1,
+            "in_stock": {"$toDouble": "$in stock"},
+            "tests_per_day": {"$toDouble": "$tests/day"},
+            "tests_per_vial": {"$toDouble": "$tests/vial"}
+        }},
+        {"$addFields": {
+            "total_tests_in_stock": {"$multiply": ["$in_stock", "$tests_per_vial"]},
+            "total_days_to_last": {"$ceil": {"$divide": ["$total_tests_in_stock", "$tests_per_day"]}},
+            "quantity_test_requested": {"$multiply": ["$tests_per_day", days]}
+        }}
+    ]
+    
+    # Execute the aggregation pipeline
     result = list(ITEMS_COLLECTION.aggregate(pipeline))
-
-     # Convert MongoDB documents to dictionaries
+    
+    # Convert MongoDB documents to dictionaries
     result_dicts = []
     for item in result:
         if item.get("total_tests_in_stock") < item.get("quantity_test_requested"):
             item["amount_needed"] = item.get("quantity_test_requested") - item.get("total_tests_in_stock")
-            amount_needed = math.ceil(item.get("amount_needed")/item.get("tests/vial"))
-            # return amount_needed
+            amount_needed = math.ceil(item.get("amount_needed") / item.get("tests_per_vial"))
         else:
             item["amount_needed"] = 0
             amount_needed = 0
-            
+
         result_dict = {
             "bench": item.get("bench", ""),
-            "in_stock": item.get("in stock", ""),
+            "in_stock": item.get("in_stock", ""),
             "item": item.get("item", ""),
-            "tests_per_day": item.get("tests/day", ""),
-            "total_tests_in_stock": item.get("total_tests_in_stock", ""),
             "tests_per_day": item.get("tests_per_day", ""),
+            "total_tests_in_stock": item.get("total_tests_in_stock", ""),
             "quantity_test_requested": item.get("quantity_test_requested", ""),
             "total_days_to_last": item.get("total_days_to_last", ""),
             "amount_needed": amount_needed
@@ -125,6 +118,7 @@ def requiste(bench, days, categories):
     
     # Return the result as JSON
     return result_dicts
+
 
 
     # Example usage:
@@ -141,12 +135,12 @@ class ItemsRequisite(Resource):
         days = args['days']
         categories = args.get('categories', [])  # Get list of categories or empty list if not provided        
         results = requiste(bench, days, categories)
+        print(results)
         response = results
         # for result in results:
 
         response = {"requested": results,}
         return response, 200
-
 
 class ItemsDeleteResource(Resource):
     def delete(self):
